3049517a125d122a984bc32e4e5ebf20716eb739
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2023-10-15 18:42:31 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Provider from './components/Provider';
+import Layout from './components/Layout';
+import { Grid, Paper } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Layout>
+          <Grid item xs={12} md={8} sx={{ minHeight: 400 }}>
+         {/* <Map>
+          <Switch>
+            <Route exact path="/">
+              <CinemaMarkers />
+            </Route>
+             <Route path="/nearby" component={NearbyCinemaMarkers} />
+             <Route
+               path="/:franchiseId/:countryCode"
+               component={CinemaMarkers}
+             />
+           </Switch>
+         </Map> */}
+       </Grid>
+       <Grid item xs={12} md={4}>
+         <Paper sx={{ p: 1 }}>
+           {/* <Switch>
+             <Route exact path="/" component={AsideIndex} />
+             <Route path="/nearby" component={NearbyCinemasList} />
+             <Route
+               path="/:franchiseId/:countryCode"
+               component={FranchiseCinemasList}
+             />
+             <Route path="*" component={NotFound} />
+           </Switch> */}
+         </Paper>
+       </Grid>
+  </Layout>
+    </Provider>
   );
 }
 
